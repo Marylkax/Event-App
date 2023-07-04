f0ac03de771098e0e1338e157faaf8d2156665d4
Marylkax <108342249+Marylka@users.noreply.github.com>
2023-07-04 07:50:24 +0100

    Added individual pages for each event

--- r-toolkit-event-app/src/App.jsx
+++ r-toolkit-event-app/src/App.jsx
@@ -3,31 +3,54 @@ import { useState, useEffect } from "react";
 import { Route, Routes, Link } from "react-router-dom";
 
 import axios from "axios";
-import "./App.css";
-import Navbar from "./Components/Navbar";
+
 import Eventlist from "./Components/Eventlist";
 import Searchbar from "./Components/Searchbar";
 import Home from "./Home";
 import Listings from "./Listings";
+import About from "./About";
+import Favourites from "./Favourites";
+import EventDetails from "./Components/EventDetails";
 
 function App() {
   return (
     <>
-      <nav className="pagenav">
-        <ul>
-          <li>
-            <Link to="/">Homepage</Link>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/listings">Explore what's on</Link>
-          </li>
-        </ul>
-      </nav>
+      {" "}
+      <div className="navbarcontainer">
+        <h1>EVENTS</h1>
+        <nav className="navbarlinks">
+          <ul>
+            <li>
+              <Link to="/">Homepage</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/listings">Explore what's on</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/favourites">Favourites</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/about" element={<About />}></Route>
         <Route exact path="/listings" element={<Listings />}></Route>
+        <Route exact path="/favourites" element={<Favourites />}></Route>
+        <Route
+          exact
+          path="/eventDetails/:eventId"
+          element={<EventDetails />}
+        ></Route>
       </Routes>
     </>
   );
